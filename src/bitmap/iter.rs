d42c16b13aea901fd69f83508a228e127670cea5
saik0 <github@saik0.net>
2022-01-10 00:07:39 -0800

    create push_unchecked, call from append

--- src/bitmap/iter.rs
+++ src/bitmap/iter.rs
@@ -200,7 +200,7 @@ impl RoaringBitmap {
             }
         };
 
-        self.insert(prev);
+        self.push_unchecked(prev);
         let mut count = 1;
 
         // It is now guaranteed that so long as the values are iterator are monotonically
@@ -210,7 +210,7 @@ impl RoaringBitmap {
             if value <= prev {
                 return Err(NonSortedIntegers { valid_until: count });
             } else {
-                self.insert(value);
+                self.push_unchecked(value);
                 prev = value;
                 count += 1;
             }
