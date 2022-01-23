03c6951860297ea4f765a8969b5491a71976b077
saik0 <github@saik0.net>
2022-01-23 02:30:58 -0800

    continue making sub more idiomatic

--- src/bitmap/store/array/simd/sub.rs
+++ src/bitmap/store/array/simd/sub.rs
@@ -17,37 +17,27 @@ pub fn sub(lhs: &[u16], rhs: &[u16]) -> Vec<u16> {
         return lhs.to_vec();
     }
 
-    let lhs_len = lhs.len();
-    let rhs_len = rhs.len();
+    let st_a = (lhs.len() / VECTOR_LENGTH) * VECTOR_LENGTH;
+    let st_b = (rhs.len() / VECTOR_LENGTH) * VECTOR_LENGTH;
 
     let mut out = vec![0; lhs.len().max(rhs.len()) + 4096];
-    let mut count = 0;
-    let mut i_a = 0;
-    let mut i_b = 0;
-    let st_a = (lhs_len / VECTOR_LENGTH) * VECTOR_LENGTH;
-    let st_b = (rhs_len / VECTOR_LENGTH) * VECTOR_LENGTH;
-
-    if (i_a < st_a) && (i_b < st_b) {
-        // this is the vectorized code path
-
-        //, v_bmax;
-        // we load a vector from A and a vector from B
-        let mut v_a: u16x8 = Simd::from_slice(&lhs[i_a..]);
-        let mut v_b: u16x8 = Simd::from_slice(&rhs[i_b..]);
+    let mut i = 0;
+    let mut j = 0;
+    let mut k = 0;
+    if (i < st_a) && (j < st_b) {
+        let mut v_a: u16x8 = Simd::from_slice(&lhs[i..]);
+        let mut v_b: u16x8 = Simd::from_slice(&rhs[j..]);
         // we have a runningmask which indicates which values from A have been
         // spotted in B, these don't get written out.
         let mut runningmask_a_found_in_b: usize = 0;
-        /****
-         * start of the main vectorized loop
-         *****/
         loop {
             // a_found_in_b will contain a mask indicate for each entry in A
             // whether it is seen in B
             let a_found_in_b: usize = to_bitmask(matrix_cmp(v_a, v_b));
             runningmask_a_found_in_b |= a_found_in_b;
             // we always compare the last values of A and B
-            let a_max: u16 = lhs[i_a + VECTOR_LENGTH - 1];
-            let b_max: u16 = rhs[i_b + VECTOR_LENGTH - 1];
+            let a_max: u16 = lhs[i + VECTOR_LENGTH - 1];
+            let b_max: u16 = rhs[j + VECTOR_LENGTH - 1];
             if a_max <= b_max {
                 // Ok. In this code path, we are ready to write our v_a
                 // because there is no need to read more from B, they will
@@ -61,35 +51,32 @@ pub fn sub(lhs: &[u16], rhs: &[u16]) -> Vec<u16> {
                 // TODO make this a cast when it's supported
                 let sm16: u16x8 = unsafe { mem::transmute(sm16) };
                 let p: u16x8 = swizzle_u16x8(v_a, sm16);
-                store(p, &mut out[count..]); // can overflow
-                count += bitmask_belongs_to_difference.count_ones() as usize;
-                // we advance a
-                i_a += VECTOR_LENGTH;
-                if i_a == st_a {
-                    // no more
+                store(p, &mut out[k..]); // can overflow
+                k += bitmask_belongs_to_difference.count_ones() as usize;
+                i += VECTOR_LENGTH;
+                if i == st_a {
                     break;
                 }
                 runningmask_a_found_in_b = 0;
-                v_a = Simd::from_slice(&lhs[i_a..]);
+                v_a = Simd::from_slice(&lhs[i..]);
             }
             if b_max <= a_max {
                 // in this code path, the current v_b has become useless
-                i_b += VECTOR_LENGTH;
-                if i_b == st_b {
+                j += VECTOR_LENGTH;
+                if j == st_b {
                     break;
                 }
-                v_b = Simd::from_slice(&rhs[i_b..]);
+                v_b = Simd::from_slice(&rhs[j..]);
             }
         }
+        // End of main vectorized loop
         // at this point, either we have i_a == st_a, which is the end of the
         // vectorized processing,
         // or we have i_b == st_b,  and we are not done processing the vector...
         // so we need to finish it off.
-        if i_a < st_a {
+        if i < st_a {
             // we have unfinished business...
-            let mut buffer: [u16; 8] = [0; 8]; // buffer to do a masked load
-            buffer[..rhs_len - i_b].copy_from_slice(&rhs[i_b..]);
-            v_b = Simd::from_array(buffer);
+            v_b = Simd::from_slice(&rhs[j..]);
             let a_found_in_b: usize = to_bitmask(matrix_cmp(v_a, v_b));
             runningmask_a_found_in_b |= a_found_in_b;
             let bitmask_belongs_to_difference: usize = runningmask_a_found_in_b ^ 0xFF;
@@ -98,36 +85,36 @@ pub fn sub(lhs: &[u16], rhs: &[u16]) -> Vec<u16> {
             // TODO make this a cast when it's supported
             let sm16: u16x8 = unsafe { mem::transmute(sm16) };
             let p: u16x8 = swizzle_u16x8(v_a, sm16);
-            store(p, &mut out[count..]);
-            count += bitmask_belongs_to_difference.count_ones() as usize;
-            i_a += VECTOR_LENGTH;
+            store(p, &mut out[k..]);
+            k += bitmask_belongs_to_difference.count_ones() as usize;
+            i += VECTOR_LENGTH;
         }
         // at this point we should have i_a == st_a and i_b == st_b
     }
     // do the tail using scalar code
     // match arms can be reordered, the ordering here is perf sensitive
     #[allow(clippy::comparison_chain)]
-    while i_a < lhs_len && i_b < rhs_len {
-        let a = lhs[i_a];
-        let b = rhs[i_b];
+    while i < lhs.len() && j < rhs.len() {
+        let a = lhs[i];
+        let b = rhs[j];
         if a > b {
-            i_b += 1;
+            j += 1;
         } else if a < b {
-            out[count] = a;
-            count += 1;
-            i_a += 1;
+            out[k] = a;
+            k += 1;
+            i += 1;
         } else {
-            i_a += 1;
-            i_b += 1;
+            i += 1;
+            j += 1;
         }
     }
 
-    // Why cant i_b be < rhs_len?
-    if i_a < lhs_len {
-        let n = lhs_len - i_a;
-        out[count..n + count].copy_from_slice(&lhs[i_a..n + i_a]);
-        count += n;
+    // Why cant i_b be < rhs.len()?
+    if i < lhs.len() {
+        let n = lhs.len() - i;
+        out[k..k + n].copy_from_slice(&lhs[i..i + n]);
+        k += n;
     }
-    out.truncate(count);
+    out.truncate(k);
     out
 }
